fd3453dd486441ac9aa29360f7e02c82bb383bcd
nodirbek <nodirbekuktamov2006@gmail.com>
2022-03-10 17:52:01 +0500

    fixes

--- frontend/src/urls.js
+++ frontend/src/urls.js
@@ -4,6 +4,6 @@ export const CONFIRM = '/users/confirm'
 export const RESET_PASSWORD = '/users/reset_password'
 
 export const TRADE = '/main/trades/'
-export const TRADE_DETAIL = '/main/trades/{id}'
+export const TRADE_DETAIL = '/main/trades/{id}/'
 
 export const domain = 'https://4287-213-230-80-253.ngrok.io' // http://62.113.98.2:8000
