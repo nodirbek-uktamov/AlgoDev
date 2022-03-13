065f05842f0ea7e034ad88ec46c942863bc30061
nodirbek <nodirbekuktamov2006@gmail.com>
2022-03-13 21:13:21 +0500

    fixes

--- backend/apps/main/routing.py
+++ backend/apps/main/routing.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from main.consumers import Consumer
+from apps.main.consumers import Consumer
 
 websocket_urlpatterns = [
     path("logs/<int:id>/", Consumer.as_asgi()),
--- backend/config/asgi.py
+++ backend/config/asgi.py
@@ -3,7 +3,7 @@ import os
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
-from main.routing import websocket_urlpatterns
+from apps.main.routing import websocket_urlpatterns
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
