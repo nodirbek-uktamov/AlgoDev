3b22a294bceeef878ed14c9f4feef334d0870fcb
nodirbek <nodirbekuktamov2006@gmail.com>
2022-03-24 12:42:02 +0500

    add admin

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -4,6 +4,7 @@ from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
+    path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
     path('api/v1/', include([
         path('core/', include(('core.urls', 'core'), namespace='core')),
