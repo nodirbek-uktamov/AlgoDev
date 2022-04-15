bc3db9904e02fd1011613ae1fd35b26f508a56ca
nodirbek <nodirbekuktamov2006@gmail.com>
2022-04-15 20:08:07 +0500

    fixes

--- frontend/src/urls.js
+++ frontend/src/urls.js
@@ -13,9 +13,7 @@ export const HUOBI_DOMAIN = 'https://api.huobi.pro'
 export const HUOBI_SYMBOLS = `${HUOBI_DOMAIN}/v2/settings/common/symbols/`
 export const HUOBI_SYMBOL_SETTINGS = `${HUOBI_DOMAIN}/v1/settings/common/symbols`
 
-const host = '194.163.164.243'
-
-export const domain = `http://${host}:8000` // http://62.113.98.2:8000
+export const domain = window.origin // https://hftcryptobot.com
 export const wsDomain = domain.replace('http', 'ws')
 
 export const LOGS_WS = `${wsDomain}/logs/{id}/`
