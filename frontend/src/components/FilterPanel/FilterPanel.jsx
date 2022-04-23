6fecd0e849523f37c18eb4cdc52f3e51934d3023
Vlad <vlad.nnov@mail.ru>
2022-04-23 21:26:14 +0300

    move code from TradesList to OrdersList

--- frontend/src/components/FilterPanel/FilterPanel.jsx
+++ frontend/src/components/FilterPanel/FilterPanel.jsx
@@ -1,14 +1,19 @@
 import React from 'react'
-import  './FilterPanel.scss'
+import './FilterPanel.scss'
 
-export function FilterPanel({handleFilter, allTradesCount, openTradeCount}) {
+export function FilterPanel({handleFilter, allOrdersCount, openOrdersCount}) {
 
     return <div className='filter-panel_container'>
-        <button onClick={handleFilter("")} className='filter-panel_btn filter-panel_btn-selected'>all ( {allTradesCount} )</button>
-        <button onClick={handleFilter('orderStatus', 'open')} className='filter-panel_btn'>open ( {openTradeCount} )</button>
+        <button onClick={handleFilter("")} className='filter-panel_btn filter-panel_btn-selected'>all
+            ( {allOrdersCount} )
+        </button>
+        <button onClick={handleFilter('orderStatus', 'open')} className='filter-panel_btn'>open ( {openOrdersCount} )
+        </button>
         <button onClick={handleFilter('orderStatus', 'filled')} className='filter-panel_btn'>filled</button>
         <button onClick={handleFilter('orderStatus', 'cancelled')} className='filter-panel_btn'>cancelled</button>
         <button onClick={handleFilter('orderStatus', 'closed')} className='filter-panel_btn'>closed</button>
-        <button onClick={handleFilter('orderStatus', 'takeprofit')} className='filter-panel_btn filter-panel_btn-stops'>takeprofit</button>
+        <button onClick={handleFilter('orderStatus', 'takeprofit')}
+                className='filter-panel_btn filter-panel_btn-stops'>takeprofit
+        </button>
     </div>
 }
