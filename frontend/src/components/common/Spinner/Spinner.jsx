e0446719a5aa13662d58b537ca950ae48ceda9bd
Vlad <vlad.nnov@mail.ru>
2022-05-08 09:06:12 +0300

    setup Spinner

---
+++ frontend/src/components/common/Spinner/Spinner.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import {classnames} from "../../../utils/string";
+import './Spinner.scss';
+
+export function Spinner({size, width = 2, color = "currentColor", className = ''}) {
+    return (
+        <svg
+            className={classnames(["spinner_main", className])}
+            xmlns="http://www.w3.org/2000/svg"
+            width={size}
+            height={size}
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke={color}
+            strokeWidth={width}
+            strokeLinecap="round"
+            strokeLinejoin="round"
+        >
+            <circle cx="12" cy="12" r="10"/>
+        </svg>
+    );
+}
