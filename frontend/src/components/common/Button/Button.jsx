7c5a57a7d0d83de10407d753dcebf6d94b664e98
Vlad <vlad.nnov@mail.ru>
2022-05-08 09:04:00 +0300

    setup Button

---
+++ frontend/src/components/common/Button/Button.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import './Button.scss';
+import {classnames} from "../../../utils/string";
+import {Spinner} from "../Spinner";
+
+const BUTTON_SIZE = {
+    S: 'button_small',
+    M: 'button_medium',
+    L: 'button_large'
+}
+
+const BUTTON_COLOR = {
+    success: 'button_success',
+    danger: 'button_danger',
+    gray: 'button_gray',
+    white: 'button_white',
+}
+
+export function Button({
+                           text,
+                           size = 'L',
+                           color = 'gray',
+                           type = 'button',
+                           isLoading = false,
+                           disabled,
+                           className = '',
+                           ...rest
+                       }) {
+    return <button type={type}
+                   disabled={isLoading || disabled}
+                   className={classnames(['button_main', BUTTON_SIZE[size], BUTTON_COLOR[color], className])}
+                   {...rest}>{isLoading ? <Spinner className="button_spinner"/> : text}</button>
+}
